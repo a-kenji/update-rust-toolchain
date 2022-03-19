313fc297311aaa702cf47fcbb1af6dc1a0d46083
Kenji <aks.kenji@protonmail.com>
2022-03-19 23:10:45 +0100

    Add: legacy toolchain compatibility

--- update-toolchain.sh
+++ update-toolchain.sh
@@ -5,23 +5,23 @@ set -euo pipefail
 # Updates a rust-toolchain file in relation to the official rust releases.
 
 
-function _current_toolchain_version() {
+# reads the channel version from the rust-toolchain.toml file
+function _curr_toolchain_version() {
 local RUST_TOOLCHAIN_FILE="$1"
-RUST_TOOLCHAIN_VERSION=$(grep -oP 'channel = "\K[^"]+' "${RUST_TOOLCHAIN_FILE}")
-echo "$RUST_TOOLCHAIN_VERSION"
+RUST_TOOLCHAIN_VERSION=$(grep -oP \
+    'channel[[:space:]]*=[[:space:]]*"\K[^"]+' \
+    "${RUST_TOOLCHAIN_FILE}")
+echo "${RUST_TOOLCHAIN_VERSION:-false}"
 }
-
 # update with new version number
 function _update_channel(){
 local RUST_TOOLCHAIN_VERSION="$1"
 sed -e "/channel/s/\".*\"/\"${RUST_TOOLCHAIN_VERSION}\"/" "${RUST_TOOLCHAIN_FILE}"
 }
-
 function _get_last_no_releases() {
     curl --silent "https://api.github.com/repos/rust-lang/rust/releases" | \
         jq '.[range(50)].tag_name' | sed -e 's/\"//g'
 }
-
 function _parse_semver() {
     local token="$1"
     local major=0
@@ -42,8 +42,6 @@ function _get_string_arrary() {
     IFS=' ' read  -r -a array <<< "$1";
     echo "${array["${2}"]}"
 }
-
-
 function _find_minor_version() {
 local MINOR_DELTA="$1"
 local RELEASES="$2"
@@ -59,7 +57,6 @@ for i in $RELEASES;do
     fi
 done
 }
-
 function _find_patch_version() {
 local MINOR_VERSION="$1"
 local RELEASES="$2"
@@ -75,7 +72,6 @@ _main() {
 # Path to the rust-toolchain file
 RUST_TOOLCHAIN_FILE="$TOOLCHAIN_FILE"
 echo RUST_TOOLCHAIN_FILE "$TOOLCHAIN_FILE"
-#RUST_TOOLCHAIN_FILE=$(echo "$TOOLCHAIN_FILE" | awk -F/ '{print $3}')
 # How many minor versions delta there should be,
 # will automatically advance patch versions.
 MINOR_DELTA="$MINOR_VERSION_DELTA"
@@ -84,9 +80,25 @@ UPDATE_PATCH="$INPUTS_UPDATE_PATCH"
 echo UPDATE_PATCH "$UPDATE_PATCH"
 UPDATE_MINOR="$INPUTS_UPDATE_MINOR"
 echo UPDATE_MINOR "$UPDATE_MINOR"
-#MINOR_DELTA=$(echo "$MINOR_VERSION_DELTA" | awk -F/ '{print $3}')
 
-RUST_TOOLCHAIN_VERSION="$(_current_toolchain_version "$RUST_TOOLCHAIN_FILE")"
+# Try to read the current toolchain version from the toolchain file
+# If we can read the version directly, then it is still the non toml version
+RUST_TOOLCHAIN_VERSION=$(_parse_semver $(cat $RUST_TOOLCHAIN_FILE))
+# 0 0 0 means we can't parse it, so either it is a toml file, or malformed
+if [[ $RUST_TOOLCHAIN_VERSION == "0 0 0" ]]; then
+    TOML=true
+    RUST_TOOLCHAIN_VERSION="$(_curr_toolchain_version "$RUST_TOOLCHAIN_FILE")"
+    echo RUST_TOOLCHAIN_VERSION ${RUST_TOOLCHAIN_VERSION}
+    # if we can't parse it here either, it is malformed
+    if [[ $RUST_TOOLCHAIN_VERSION == "false" ]]; then
+        echo "Can't parse the toolchain file : ${RUST_TOOLCHAIN_FILE}"
+        exit
+    fi
+else
+    TOML=false
+fi
+
+
 echo CURRENT_RUST_TOOLCHAIN_VERSION "$RUST_TOOLCHAIN_VERSION"
 RUST_TOOLCHAIN_VERSION=($(_parse_semver $(echo "$RUST_TOOLCHAIN_VERSION")))
 echo RUST_TOOLCHAIN_VERSION_SEMVER "${RUST_TOOLCHAIN_VERSION[@]}"
@@ -102,9 +114,13 @@ if [[ $UPDATE_MINOR == "true" ]]; then
     VERSION=$(_find_minor_version "${MINOR_DELTA}" "$RELEASES")
 fi
 
+if [[ $TOML == "true" ]]; then
 echo "$(_update_channel $VERSION)" > "${RUST_TOOLCHAIN_FILE}"
-cat "${RUST_TOOLCHAIN_FILE}"
-
+else
+    echo "$VERSION" > "${RUST_TOOLCHAIN_FILE}"
+fi
+echo $VERSION
+echo TOML $TOML
 }
 
 
