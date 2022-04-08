0ebd49c5180916eb226821fcc50ae25675cde3e2
a-kenji <aks.kenji@protonmail.com>
2022-04-09 00:31:02 +0200

    fix: sed invocation

--- update-toolchain.sh
+++ update-toolchain.sh
@@ -16,7 +16,7 @@ echo "${RUST_TOOLCHAIN_VERSION:-false}"
 # update with new version number
 function _update_channel(){
 local RUST_TOOLCHAIN_VERSION="$1"
-sed -e "/channel/s/\".*\"/\"${RUST_TOOLCHAIN_VERSION}\"/" "${RUST_TOOLCHAIN_FILE}"
+sed -e "/channel/s/\"\.*\"/\"${RUST_TOOLCHAIN_VERSION}\"/" "${RUST_TOOLCHAIN_FILE}"
 }
 function _get_last_no_releases() {
     curl --silent "https://api.github.com/repos/rust-lang/rust/releases" | \
