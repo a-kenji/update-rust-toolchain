518f2463d788ea146a5c91b78b7cac21d54ff991
a-kenji <aks.kenji@protonmail.com>
2022-10-05 13:59:04 +0200

    move: manifest updater to a-kenji/rust-toolchain-manifest

--- flake.nix
+++ flake.nix
@@ -1,42 +1,23 @@
 {
   description = "update-rust-action";
 
-  inputs.rust-overlay = {
-    url = "github:oxalica/rust-overlay";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.flake-utils.follows = "flake-utils";
+  inputs = {
+    toolchain-manifest.url = "github:a-kenji/rust-toolchain-manifest";
+    toolchain-manifest.inputs.nixpkgs.follows = "nixpkgs";
+    toolchain-manifest.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    rust-overlay,
+    toolchain-manifest,
   }:
     flake-utils.lib.eachDefaultSystem
     (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      overlays = [(import rust-overlay)];
-      rustPkgs = import nixpkgs {
-        inherit system overlays;
-      };
-      CARGO_LOCK = "${./.}/Cargo.lock";
-      CARGO_TOML = "${./.}/Cargo.toml";
-      RUST_TOOLCHAIN = "${./.}/rust-toolchain.toml";
-      rustToolchainToml = rustPkgs.rust-bin.fromRustupToolchainFile "${RUST_TOOLCHAIN}";
-      rustc = rustToolchainToml;
-      cargo = rustToolchainToml;
-
-      buildInputs = [
-        pkgs.openssl
-      ];
-      nativeBuildInputs = [
-        pkgs.pkg-config
-      ];
       devInputs = [
-        rustc
-        cargo
-        pkgs.python310
+        pkgs.python311
       ];
       shellInputs = [
         pkgs.shellcheck
@@ -50,56 +31,38 @@
         pkgs.writeScriptBin "update-${channel}" ''
           set -x
            git config --local user.name "github-actions[bot]"
-           # create temporary directory for downloads
-           export TMP_DIR=./tmplocal
-           mkdir $TMP_DIR
-           # export WORKERS=5
            nix run -L github:$GITHUB_REPOSITORY \
              --no-write-lock-file \
              -- \
-             --output ./outputs \
+             --version
+           nix run -L github:$GITHUB_REPOSITORY \
+             --no-write-lock-file \
+             -- \
+             --output ./. \
              ${channel}
            git add .
            git commit -m "$(date)"
            git push
         '';
     in {
-      devShells.default = pkgs.mkShell {
-        name = "update-rust-action-env";
-        buildInputs = shellInputs ++ fmtInputs ++ devInputs ++ buildInputs ++ nativeBuildInputs;
-      };
-      packages = {
-        default =
-          (
-            pkgs.makeRustPlatform {
-              inherit cargo rustc;
-            }
-          )
-          .buildRustPackage {
-            cargoDepsName = "update-rust-toolchain";
-            name = "update-rust-toolchain";
-            version = "0.1.0";
-            src = ./.;
-            cargoLock = {
-              lockFile = builtins.path {
-                path = ./. + "/Cargo.lock";
-                name = "Cargo.lock";
-              };
-            };
-            inherit nativeBuildInputs buildInputs;
-          };
-        ci = {
-          update-nightly = update-channel "nightly";
-          update-beta = update-channel "beta";
+      devShells = {
+        default = pkgs.mkShell {
+          name = "update-rust-action-env";
+          buildInputs = shellInputs ++ fmtInputs ++ devInputs;
         };
+        fmtShell = pkgs.mkShell {
+          buildInputs = fmtInputs;
+        };
+      };
+      packages.default = toolchain-manifest.outputs.packages.${system}.default;
+      ci = {
+        update-nightly = update-channel "nightly";
+        update-beta = update-channel "beta";
       };
 
       formatter = pkgs.alejandra;
 
       checks = {
-        packages = {
-          inherit (self.outputs.packages.${system}) default;
-        };
         devShells = {
           inherit (self.outputs.devShells.${system}) default;
         };
